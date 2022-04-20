649cc9010c619c0263f89d633b6ad4928e8dc5ac
zhangyongxiang <zhangyongxiang@baidu.com>
2022-04-20 17:31:13 +0800

    update

---
+++ public/app.js
@@ -0,0 +1,61 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    } else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('http://localhost:8884/endpoint/ability');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/ability/6393913312429473792/1/2/3.4/property/2234', function (greeting) {
+            //  showGreeting(JSON.parse(greeting.body).content);
+            showGreeting(greeting.body);
+        });
+        stompClient.subscribe('/app/ability/6393913312429473792/1/2/3.4/property/2234', function (greeting) {
+            //  showGreeting(JSON.parse(greeting.body).content);
+            showGreeting(greeting.body);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/greeting", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $("#connect").click(function () {
+        connect();
+    });
+    $("#disconnect").click(function () {
+        disconnect();
+    });
+    $("#send").click(function () {
+        sendName();
+    });
+});
+
